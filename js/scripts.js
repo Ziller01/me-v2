41b7a198511b4a9309f05e944745ec31bbf6313a
Ziller <memo3rb2002@gmail.com>
2025-10-15 23:18:06 +0300

    Improves

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,54 @@ const nav = document.getElementById("navbar");
 window.addEventListener("scroll", () => {
     const curr = window.scrollY;
     if (curr <= 0) nav.style.transform = "translateY(0)";
-    else if (curr > lastScroll) nav.style.transform = "translateY(-100%)";
+    else if (curr > lastScroll) nav.style.transform = "translateY(-100px)";
     else nav.style.transform = "translateY(0)";
     lastScroll = curr;
 });
 
+function togglePhoneList() {
+    const menuPhone = document.getElementById("menuPhone");
+    menuPhone && menuPhone.classList.toggle("hidden")
+}
 
-const menuBtn = document.getElementById("menuBtn");
-menuBtn && menuBtn.addEventListener("click", () => { alert("Mobile menu — implement as needed"); });
+// ! >>==================================>> Effects
+
+const lastSection = document.querySelector("#contact"); // ضع ID آخر قسم عندك
+let fired = false;
+
+window.addEventListener("scroll", () => {
+    const rect = lastSection.getBoundingClientRect();
+    if (!fired && rect.top < window.innerHeight * 0.8) {
+        fired = true;
+        const settings = {
+            count: 150,
+            spread: 100,
+            velocity: 50,
+            decay: 0.9,
+            gravity: 1,
+            colors: ['#73a2fd', '#76db7f', '#e46e6c', '#ffffff'],
+        };
+
+        const heart = confetti.shapeFromPath({
+            path: 'M10 30 A20 20 0 0 1 50 30 A20 20 0 0 1 90 30 Q90 60 50 90 Q10 60 10 30Z'
+        });
+
+
+        confetti({
+            particleCount: settings.count,
+            spread: settings.spread,
+            startVelocity: settings.velocity,
+            decay: settings.decay,
+            gravity: settings.gravity,
+            colors: settings.colors,
+            shapes: [heart],
+            scalar: 1.2,
+            origin: { y: 0.8 }
+        });
+
+    }
+    else if (fired && rect.top > window.innerHeight * 0.8) { fired = false }
+});
 
 // ! >>==================================>> Animations
 
@@ -35,9 +75,70 @@ document.querySelectorAll(".animate-fadeUp").forEach(el => {
     obs.observe(el);
 });
 
-// ! >>==================================>> Footer
+// ! >>==================================>> Dynamic Skills
 
-document.getElementById("year").textContent = `2023-${new Date().getFullYear()}`;
+const skills = [
+    {
+        name: "HTML",
+        persent: "80",
+        color: "bg-orange-500",
+        bgColor: "bg-orange-500/10 border-orange-500/10",
+    },
+    {
+        name: "TailwindCSS",
+        persent: "85",
+        color: "bg-sky-600",
+        bgColor: "bg-sky-600/10 border-sky-600/10",
+    },
+    {
+        name: "JavaScript",
+        persent: "90",
+        color: "bg-yellow-500",
+        bgColor: "bg-yellow-500/10 border-yellow-500/10",
+    },
+    {
+        name: "React",
+        persent: "80",
+        color: "bg-sky-400",
+        bgColor: "bg-sky-400/10 border-sky-400/10",
+    },
+    {
+        name: "Next.js",
+        persent: "75",
+        color: "bg-gray-500",
+        bgColor: "bg-gray-500/10 border-gray-500/10",
+    },
+    {
+        name: "NodeJS",
+        persent: "95",
+        color: "bg-green-600",
+        bgColor: "bg-green-600/10 border-green-600/10",
+    },
+];
+
+const skills_container = document.getElementById("skills-container");
+skills_container.innerHTML = ""
+
+skills.forEach(s => {
+    skills_container.innerHTML += `
+    <article class="${s.bgColor} border rounded-xl p-5 animate-fadeUp">
+        <div class="flex items-center justify-between mb-3">
+            <div class="text-sm text-gray-300">${s.name}</div>
+            <div class="text-xs text-gray-400">${s.persent}%</div>
+        </div>
+        <div class="w-full bg-white/5 h-2 rounded-full overflow-hidden">
+            <div class="h-2 rounded-full ${s.color}" style="width: ${s.persent}%;"></div>
+        </div>
+    </article>
+    `;
+    // <div class="bg-gray-600/10 rounded-xl p-6 text-center animate-fadeUp">
+    //     <div class="text-lg font-medium text-white">Tailwind</div>
+    //     <p class="text-sm text-gray-300 mt-3">Utility-first CSS for rapid UI.</p>
+    // </div>
+
+});
+
+
 // ! >>==================================>> Dynamic Projects
 
 const statuses = {
@@ -45,6 +146,7 @@ const statuses = {
     beta: { name: "BETA", color: "bg-lime-500/20 text-lime-400", cardColor: "bg-lime-500/5 border-lime-500/10" },
     inProgress: { name: "IN PROGRESS", color: "bg-yellow-500/20 text-yellow-400", cardColor: "bg-yellow-500/5 border-yellow-500/10" },
     idle: { name: "IDLE", color: "bg-gray-500/20 text-gray-400", cardColor: "bg-gray-500/5 border-gray-500/10" },
+    old: { name: "OLD Verion", color: "bg-gray-500/20 text-gray-400", cardColor: "bg-gray-500/5 border-gray-500/10" },
     canceled: { name: "CANCELLED", color: "bg-red-500/20 text-red-400", cardColor: "bg-red-500/5 border-red-500/10" },
 }
 
@@ -54,7 +156,8 @@ const projects = [
         desc: "Three.js experiment.",
         image: "../media/web_primedentalclinic.png",
         link: "https://primedentalclinic.net/",
-        tech: ["HTML", "Tailwind v3", "JS"],
+        createDate: '11-2024',
+        skills: ["HTML", "TailwindCSS", "JS"],
         tags: ["Clinic"],
         status: statuses.completed,
     },
@@ -63,7 +166,8 @@ const projects = [
         desc: "Three.js experiment.",
         image: "../media/web_eslam-portfolio.png",
         link: "https://dreslamnubi.com/",
-        tech: ["HTML", "Tailwind v4", "JS"],
+        createDate: '5-2025',
+        skills: ["HTML", "TailwindCSS", "JS"],
         tags: ["Portfolio"],
         status: statuses.completed,
     },
@@ -72,7 +176,8 @@ const projects = [
         desc: "Personal site built with Tailwind.",
         image: "../media/web_abdelmawla.png",
         link: "https://abdelmawla.site/",
-        tech: ["HTML", "Tailwind v4", "JS"],
+        createDate: '9-2025',
+        skills: ["HTML", "TailwindCSS", "JS"],
         tags: ["Portfolio"],
         status: statuses.completed,
     },
@@ -81,9 +186,30 @@ const projects = [
         desc: "Interactive AI admin panel.",
         image: "../media/web_kareem.png",
         link: "https://xrk111.github.io/kareem/",
-        tech: ["HTML", "Tailwind v4", "JS"],
+        createDate: '9-2025',
+        skills: ["HTML", "TailwindCSS", "JS"],
         tags: ["Portfolio"],
         status: statuses.inProgress,
+    },
+    {
+        name: "TempBot",
+        desc: "Discord bot for managing temproles.",
+        image: "../media/web_tempbot.png",
+        link: "https://www.tempbot.xyz",
+        createDate: '',
+        skills: ["Node.js", "MySQL", "discord.js", "REST API", "HTML", "TailwindCSS", "JS"],
+        tags: ["Discord Bot"],
+        status: statuses.idle,
+    },
+    {
+        name: "Ahmed Talaat Portfolio",
+        desc: "My old personal portfolio.",
+        image: "../media/web_ahmedtalaat.png",
+        link: "https://Ziller01.github.io/me",
+        createDate: '9-2025',
+        skills: ["HTML", "TailwindCSS", "JS"],
+        tags: ["Portfolio"],
+        status: statuses.old,
     },
 ];
 
@@ -99,92 +225,31 @@ projects.forEach(p => {
             <h4 class="font-semibold text-xl text-white">${p.name}</h4>
             <p class="font-normal text-sm text-gray-300">${p.desc}</p>
             <div class="flex gap-3 flex-wrap font-medium text-sm">
-                ${p.tech.map(m => `<span class="text-gray-500/80">${m}</span>`).join("\n")}
-                ${p.tech.length > 3 ? `<span class="text-gray-600/80">+${p.tech.length - 3} more</span>` : ""}
+                ${p.skills.slice(0, 3).map(m => `<span class="text-gray-500/80">${m}</span>`).join("\n")}
+                ${p.skills.length > 3 ? `<span class="cursor-pointer group text-gray-600/80" data-dropdown="${p.skills.slice(3)}">+${p.skills.length - 3} more</span>` : ""}
             </div>
         </div>
 
         
         <div class="flex justify-between gap-3">
-            <span class="w-fit text-sm px-2 py-1 font-light rounded-md ${p.status.color}">${p.status.name}</span>
-            <a href="${p.link}" class="text-sm px-3 py-1 bg-cyan-600 text-black flex gap-3 rounded-md"><i class="bi bi-box-arrow-up-right"></i> Go Live</a>
+            <span class="w-fit text-sm px-2 py-1 font-light rounded-full uppercase ${p.status.color}">${p.status.name}</span>
+            <a href="${p.link}" target="_blank" class="text-sm px-3 py-1 bg-cyan-400/70 border border-cyan-400/50 hover:bg-cyan-400 hover:shadow-all-md shadow-cyan-400 text-black flex gap-3 rounded-full duration-100"><i class="bi bi-box-arrow-up-right"></i> Go Live</a>
         </div>
     </article>
     `;
 });
 
-// ! >>==================================>> Dynamic Skills
+// ! >>==================================>> Dropdown Menu
 
-const skills = [
-    {
-        name: "Prime Dental Clinic",
-        desc: "Three.js experiment.",
-        image: "../media/web_primedentalclinic.png",
-        link: "https://primedentalclinic.net/",
-        tech: ["HTML", "Tailwind v3", "JS"],
-        tags: ["Clinic"],
-        status: statuses.completed,
-    },
-    {
-        name: "Eslam Portfolio",
-        desc: "Three.js experiment.",
-        image: "../media/web_eslam-portfolio.png",
-        link: "https://dreslamnubi.com/",
-        tech: ["HTML", "Tailwind v4", "JS"],
-        tags: ["Portfolio"],
-        status: statuses.completed,
-    },
-    {
-        name: "Abdelmawla Portfolio",
-        desc: "Personal site built with Tailwind.",
-        image: "../media/web_abdelmawla.png",
-        link: "https://abdelmawla.site/",
-        tech: ["HTML", "Tailwind v4", "JS"],
-        tags: ["Portfolio"],
-        status: statuses.completed,
-    },
-    {
-        name: "Kareem Portfolio",
-        desc: "Interactive AI admin panel.",
-        image: "../media/web_kareem.png",
-        link: "https://xrk111.github.io/kareem/",
-        tech: ["HTML", "Tailwind v4", "JS"],
-        tags: ["Portfolio"],
-        status: statuses.inProgress,
-    },
-];
-
-const skills_container = document.getElementById("skills-container");
-skills_container.innerHTML = ""
-
-skills.forEach(p => {
-    skills_container.innerHTML += `
-    <article class="bg-gray-500/10 rounded-xl p-5 flex flex-col gap-5 animate-fadeUp border border-white/10">
-        <img src="${p.image}" alt="project" class="w-full aspect-video object-cover object-top rounded-md" />
-
-        <div class="flex flex-col gap-1">
-            <h4 class="font-semibold text-xl text-white">${p.name}</h4>
-            <p class="font-normal text-sm text-gray-300">${p.desc}</p>
-            <div class="flex gap-3 flex-wrap font-medium text-sm">
-                ${p.tech.map(m => `<span class="text-gray-500/80">${m}</span>`).join("\n")}
-                ${p.tech.length > 3 ? `<span class="text-gray-600/80">+${p.tech.length - 3} more</span>` : ""}
-            </div>
+document.querySelectorAll("[data-dropdown]").forEach(el => {
+    const key = el.dataset.dropdown;
+    const items = key.split(',');
+    if (!items) return;
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `
+        <div class="absolute hidden z-10 group-hover:flex gap-1 -translate-x-10 bg-black/20 backdrop-blur-sm text-white/70 font-light text-sm border border-white/10 rounded-md shadow-lg overflow-hidden">
+            ${items.map(i => `<p class="whitespace-nowrap px-2 py-1">${i}</p>`).join("")}
         </div>
-
-        
-        <div class="flex justify-between gap-3">
-            <span class="w-fit text-sm px-2 py-1 font-light rounded-md ${p.status.color}">${p.status.name}</span>
-            <a href="${p.link}" class="text-sm px-3 py-1 bg-cyan-600 text-black flex gap-3 rounded-md"><i class="bi bi-box-arrow-up-right"></i> Go Live</a>
-        </div>
-    </article>
-    <article class="bg-blue-800/10 rounded-xl p-5 animate-fadeUp">
-        <div class="flex items-center justify-between mb-3">
-            <div class="text-sm text-gray-300">HTML / Tailwind</div>
-            <div class="text-xs text-gray-400">70%</div>
-        </div>
-        <div class="w-full bg-white/5 h-2 rounded-full overflow-hidden">
-            <div class="h-2 rounded-full" style="width: 70%; background: linear-gradient(90deg, #065fd4, #388bf8)"></div>
-        </div>
-    </article>
     `;
+    el.appendChild(wrapper);
 });
